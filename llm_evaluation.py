8dd23aa8a788b7976f11ab19f4f520e8737feee5
selmanbaysan <mselmanbaysan@gmail.com>
2025-02-10 13:40:36 +0300

    evaluation codes are added

--- llm_evaluation.py
+++ llm_evaluation.py
@@ -1,24 +1,29 @@
-from vllm import LLM, SamplingParams
+#from vllm import LLM, SamplingParams
+import requests
+import json
 
-class Translation:
-    def __init__(self, model_path="CohereForAI/aya-expanse-8b", prompt_path="prompts/translation_evaluation_prompt.txt"):
-        self.model = LLM(model=model_path)
-        self.sampling_params = SamplingParams(use_beam_search=True, best_of=3, max_tokens=512, length_penalty=1.2, temperature=0)
-        self.evaluation_prompt = self.read_prompt(prompt_path)
-
-    def read_prompt(self, prompt_path):
+class Evaluation:
+    def __init__(self, model_path="CohereForAI/aya-expanse-8b", prompt_path="prompts/translation_evaluation_prompt_v2.txt"):
+        #self.model = LLM(model=model_path)
+        #self.sampling_params = SamplingParams(use_beam_search=True, best_of=3, max_tokens=512, length_penalty=1.2, temperature=0)
         with open(prompt_path, "r") as f:
-            self.translate_prompt = f.read()
-        
-    def translate(self, text):
-        input_prompt = self.translate_prompt.format(text)
-        output = self.model.generate(input_prompt, self.sampling_params)
-        return output.text
+            self.evaluation_prompt = f.read()
+        self.url = "http://localhost:11434/api/generate"
+    
+    def evaluate(self, original_text, translated_text):
+        data = {
+            "model": "aya-expanse:latest",
+            "prompt": self.evaluation_prompt.format(original_text=original_text, translated_text=translated_text),
+            "stream": False,
+            "required": ["decision"]
+        }
+        response = requests.post(self.url, json=data)
+        return json.loads(response.text)['response']
 
 
 if __name__ == "__main__":
-    translator = Translation()
-    print(translator.translate("Hello, how are you?"))
+    evaluator = Evaluation()
+    print(evaluator.evaluate("Hello, how are you?", "Merhaba, naber?"))
 
 
     
